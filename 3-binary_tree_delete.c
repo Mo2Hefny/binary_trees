948639509a8dc6ad089c8d304df8587b1de65575
Mo2Hefny <moamensaied1@gmail.com>
2023-02-27 19:43:46 +0200

    betty

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,16 +2,16 @@
 
 /**
  * binary_tree_delete - deletes an entire binary tree.
- * 
+ *
  * @tree: pointer to the root node of the tree to delete.
 */
 void binary_tree_delete(binary_tree_t *tree)
 {
 if (!tree)
 return;
-if(tree->left)
+if (tree->left)
 binary_tree_delete(tree->left);
-if(tree->right)
+if (tree->right)
 binary_tree_delete(tree->right);
 free(tree);
 }
