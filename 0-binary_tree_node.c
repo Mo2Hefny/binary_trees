8633892c3e30763a0df9505ec674f5f474a1ee84
Mo2Hefny <moamensaied1@gmail.com>
2023-02-27 19:12:42 +0200

    0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+  binary_tree_t *New = malloc(sizeof(binary_tree_t));
+  if (!New)
+    return NULL;
+  New->n = value;
+  New->parent = parent;
+  New->left = NULL;
+  New->right = NULL;
+  return New;
+}
